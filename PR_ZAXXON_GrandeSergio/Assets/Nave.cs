53c7e8e8720c6f3b62db3e66fec5e279ffa11803
SergioGrande <sergio.grande.her@gmail.com>
2021-10-01 19:39:32 +0200

    Movimiento_nave

--- PR_ZAXXON_GrandeSergio/Assets/Nave.cs
+++ PR_ZAXXON_GrandeSergio/Assets/Nave.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Nave : MonoBehaviour
 {
     public float desplSpeed;
+    public float rotationSpeed;
+
+
+
+
+    //Restricción
+    float limiteH = 20;
+
     // Start is called before the first frame update
     void Start()
     {
         desplSpeed = 10f;
+        rotationSpeed = 15f;
     }
 
     // Update is called once per frame
@@ -17,12 +26,38 @@ public class Nave : MonoBehaviour
 
 
         float desplx = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * desplSpeed*desplx);
-
         float desply = Input.GetAxis("Vertical");
+        float desplr = Input.GetAxis("Rotacion");
+
+        float posx = transform.position.x;
+        float posy = transform.position.y;
+
+        transform.Translate(Vector3.right * Time.deltaTime * desplSpeed*desplx);
         transform.Translate(Vector3.up * Time.deltaTime * desplSpeed * desply);
 
+        //transform.Rotate(0f, 0f, desplr *Time.deltaTime*-rotationSpeed);
 
+        if(posx > 20f && desplx > 0)
+        {
+            transform.position = new Vector3(posx, transform.position.y, transform.position.z);
+        }
 
+        if (posx < -20f && desplx < 0)
+        {
+            transform.position = new Vector3(posx, transform.position.y, transform.position.z);
+        }
+
+        if (posy > 15f && desply > 0)
+        {
+            transform.position = new Vector3(transform.position.x, posy, transform.position.z);
+        }
+
+        if (posy < 0f && desply < 0)
+        {
+            transform.position = new Vector3(transform.position.x, posy, transform.position.z);
+        }
+        
+
+       
     }
 }
---
+++ PR_ZAXXON_GrandeSergio/Assets/Scripts/Camara.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    [SerializeField] Transform playerPosition;
+    [SerializeField] float smoothVelocity = 0.3F;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerPosition.position.x, playerPosition.position.y, playerPosition.position.z - 15);
+    }
+}
